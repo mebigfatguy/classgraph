36cfac5faa1848906e00a22d4cf5d04bacac121c
Dave Brosius <dbrosius@mebigfatguy.com>
2015-07-31 21:00:15 -0400

    put different weights on different kinds of relationships

---
+++ src/com/mebigfatguy/classgraph/RelationshipType.java
@@ -0,0 +1,33 @@
+/*
+ * ClassGraph - a dependency graph display
+ * Copyright 2013-2015 MeBigFatGuy.com
+ * Copyright 2013-2015 Dave Brosius
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package com.mebigfatguy.classgraph;
+
+public enum RelationshipType {
+	INHERITANCE(4.0f), INTERFACE(3.0f), INNER(2.0f), CONTAINMENT(1.5f), UNKNOWN(1.0f);
+
+	private float relationshipWeight;
+
+	private RelationshipType(float weight) {
+		relationshipWeight = weight;
+	}
+
+	public float getWeight() {
+		return relationshipWeight;
+	}
+
+}
